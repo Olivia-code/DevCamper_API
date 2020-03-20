89bbcc71e011d4d0628863e6117d60d8518ca2ec
Olivia <olivia.devops@gmail.com>
2020-03-20 00:05:15 -0700

    Initial bootcamp routes and controller

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+//Route files
+
+const bootcamps = require('./routes/bootcampes');
 
 //LOad env Vars
 
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Mount Routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
