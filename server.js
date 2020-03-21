1854c1129edeca6b5cae2d1069ca7b4e869bafb7
Olivia <olivia.devops@gmail.com>
2020-03-21 12:10:14 -0700

    Mongoose Connect

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
+
+//Lado env Vars
+
+dotenv.config({ path: './config/config.env' });
+
+// connect to database
+connectDB();
 
 //Route files
 
 const bootcamps = require('./routes/bootcampes');
-
-//LOad env Vars
-
-dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
@@ -22,7 +27,17 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
+
+// Handles unhandled promise rejections
+
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red.bold);
+  // close Server $ exit process
+  server.close(() => process.exit(1));
+});
