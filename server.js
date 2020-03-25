52fca36e7e474b5941020c4fce442c12b78afeb0
Olivia <olivia.devops@gmail.com>
2020-03-24 20:54:45 -0700

    Course routes & getCourses

--- server.js
+++ server.js
@@ -2,19 +2,18 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
-const errorHandler = require('./middleware/error')
+const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
-//Lado env Vars
-
+// Load env vars
 dotenv.config({ path: './config/config.env' });
 
-// connect to database
+// Connect to database
 connectDB();
 
-//Route files
-
+// Route files
 const bootcamps = require('./routes/bootcamps');
+const courses = require('./routes/courses');
 
 const app = express();
 
@@ -26,10 +25,11 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-// Mount Routers
+// Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
+app.use('/api/v1/courses', courses);
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
@@ -40,10 +40,9 @@ const server = app.listen(
   )
 );
 
-// Handles unhandled promise rejections
-
+// Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`.red.bold);
-  // close Server $ exit process
-  server.close(() => process.exit(1));
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  // server.close(() => process.exit(1));
 });
